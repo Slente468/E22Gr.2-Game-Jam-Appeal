f1d500fcc824bbe502b55a56665e2ac289303921
marievolsen <mvol22@student.aau.dk>
2022-11-24 10:57:42 +0100

    bullets and stuffs

---
+++ Gr.2App-eel/Assets/Scripts/EnemyBulletScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletScript : MonoBehaviour
+{
+    //Code credit '2D Enemy Shooting Unity Tutorial' by MoreBBlakeyyy on Youtube
+    private GameObject player;
+    private Rigidbody2D rb;
+    public float force;
+    private float timer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
